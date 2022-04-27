b90440a8946010990851cca256a86d930b78559c
Divya Gompa <gompadivya22@gmail.com>
2022-04-26 22:56:32 -0700

    Task 17 - Added Random generator button

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,22 +1,61 @@
 package View;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
 
-    Main() {
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800, 800);
+    public Main() {
+        JFrame frame = new JFrame();
+
+        Canvas canvas = new Canvas();
+        canvas.setSize(500, 500);
+        canvas.setVisible(true);
+        canvas.setForeground(Color.DARK_GRAY);
+
+        JButton randomGenButton = new JButton();
+        randomGenButton.setVisible(true);
+        randomGenButton.setText("Random Generator");
+
+
+        JPanel canvasPanel = new JPanel();
+        canvasPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        canvasPanel.setLayout(new GridLayout(0, 1));
+        canvasPanel.setSize(500, 500);
+        canvasPanel.add(canvas);
+
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        buttonsPanel.setLayout(new GridLayout(0, 5));
+        buttonsPanel.setSize(500, 50);
+        buttonsPanel.add(randomGenButton);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(canvasPanel);
+        mainPanel.add(buttonsPanel);
+
+        frame.add(mainPanel, BorderLayout.CENTER);
+
+        frame.setTitle("SRSproject");
+        frame.pack();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+
+
+
     }
 
+
     public static void main(String[] args) {
-        Canvas canvas = new Canvas();
+
         Main main = new Main();
-        main.add(canvas, BorderLayout.CENTER);
-        main.setVisible(true);
+        //main.setSize(800, 800);
+        //main.setVisible(true);
     }
+
 
 }
